95a96ec5cc50aa0a06a57fd0440b6ae5a8bbb845
NetMaster1 <bizon-nn@yandex.ru>
2024-09-28 12:22:00 +0300

    local

--- app_products/admin.py
+++ app_products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Smartphone
+from . models import Smartphone, Monitor
 
 
 # class NotebookAdmin(admin.ModelAdmin):
@@ -15,6 +15,12 @@ class SmartphoneAdmin(admin.ModelAdmin):
     #list_editable= ('processor', 'authentification', )
     #search_fields = ('imei', )
 
+class MonitorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)  
+    #list_filter = ('name',)
+    #list_editable= ('processor', 'authentification', )
+    #search_fields = ('imei', )
+
 # class TV_SetAdmin(admin.ModelAdmin):
 #     list_display = ('id', 'name', 'category_name',  'EAN')  
 #     list_filter = ('name',)
@@ -22,5 +28,6 @@ class SmartphoneAdmin(admin.ModelAdmin):
 #     #search_fields = ('imei', )
 
 admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(Monitor, MonitorAdmin)
 # admin.site.register(Notebook, NotebookAdmin)
 # admin.site.register(TV_Set, TV_SetAdmin)
